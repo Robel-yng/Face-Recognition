fcca4c1800e325a4c64bda605f2f7ad5b1bcc8f2
Robel-yng <robelnegash2020@gmail.com>
2020-06-12 20:41:23 -0400

    added more components for text, url and updated background

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react'
 import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Navigation/Logo/Logo'
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
+import Rank from './Components/Rank/Rank'
 import './App.css';
+import Particles from 'react-particles-js';
+
+const ParticlesOptions = {
+
+    particles: {
+        line_linked: {
+            shadow: {
+                enable: true,
+                color: "#3CA9D1",
+                blur: 5
+            }
+        }
+    }
+
+}
 
 class App extends Component {
     render() {
         return ( <
             div className = "App" >
             <
-            Navigation / > {
-                /*<Logo/>
-                            <ImageLinkForm/>
-                            <FaceRecognition/>*/
-            } <
+            Particles className = 'particles'
+            params = { ParticlesOptions }
+            /> <
+            Navigation / >
+            <
+            Logo / >
+            <
+            Rank / >
+            <
+            ImageLinkForm / > { /* <FaceRecognition/>*/ } <
             /div>
         )
     }
