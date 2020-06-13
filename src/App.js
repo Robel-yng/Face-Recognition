0a7f3a3e51cc21d1a86e35c73c55f66c87960bed
Robel-yng <robelnegash2020@gmail.com>
2020-06-13 18:35:42 -0400

    added face box for the face detection

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 import Particles from 'react-particles-js';
 import Clarifai from 'clarifai';
 import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+import 'tachyons'
 
 const app = new Clarifai.App({
     apiKey: 'e9da551d92854817bb17aac350b41201'
@@ -31,10 +32,28 @@ class App extends Component {
         super();
         this.state = {
             input: '',
-            imageUrl: ''
+            imageUrl: '',
+            box: {}
         }
     }
 
+    calculateFaceLocation = (data) => {
+        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+        const image = document.getElementById('inputimage');
+        const width = Number(image.width);
+        const height = Number(image.height);
+        return {
+            leftCol: clarifaiFace.left_col * width,
+            topRow: clarifaiFace.top_row * height,
+            rightCol: width - (clarifaiFace.right_col * width),
+            bottomRow: height - (clarifaiFace.bottom_row * height)
+        }
+    }
+
+    displayFaceBox = (box) => {
+        console.log(box)
+        this.setState({ box: box })
+    }
     onInputChange = (event) => {
         this.setState({ input: event.target.value })
 
@@ -42,16 +61,14 @@ class App extends Component {
 
     onButtonSubmit = () => {
         this.setState({ imageUrl: this.state.input })
-        app.models.predict(
-            Clarifai.FACE_DETECT_MODEL, this.state.input).then(
-            function(response) {
-                // do something with response
-                console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
-            },
-            function(err) {
-                // there was an error
-            }
-        );
+        app.models
+            .predict(
+                Clarifai.FACE_DETECT_MODEL,
+                this.state.input)
+            .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+            // do something with response
+            .catch(err => console.log(err))
+
     }
     render() {
         return ( <
@@ -69,7 +86,8 @@ class App extends Component {
             ImageLinkForm onInputChange = { this.onInputChange }
             onButtonSubmit = { this.onButtonSubmit }
             /> <
-            FaceRecognition imageUrl = { this.state.imageUrl }
+            FaceRecognition box = { this.state.box }
+            imageUrl = { this.state.imageUrl }
             /> <
             /div>
         )
