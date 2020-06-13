919c62760e1ba4c48cef4c68fae935893a76dadc
Robel-yng <robelnegash2020@gmail.com>
2020-06-12 23:32:36 -0400

    installed face recognition api and added face recognition component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
 import Rank from './Components/Rank/Rank'
 import './App.css';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+
+const app = new Clarifai.App({
+    apiKey: 'e9da551d92854817bb17aac350b41201'
+});
 
 const ParticlesOptions = {
 
@@ -21,6 +27,32 @@ const ParticlesOptions = {
 }
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+            imageUrl: ''
+        }
+    }
+
+    onInputChange = (event) => {
+        this.setState({ input: event.target.value })
+
+    }
+
+    onButtonSubmit = () => {
+        this.setState({ imageUrl: this.state.input })
+        app.models.predict(
+            Clarifai.FACE_DETECT_MODEL, this.state.input).then(
+            function(response) {
+                // do something with response
+                console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
+            },
+            function(err) {
+                // there was an error
+            }
+        );
+    }
     render() {
         return ( <
             div className = "App" >
@@ -34,7 +66,11 @@ class App extends Component {
             <
             Rank / >
             <
-            ImageLinkForm / > { /* <FaceRecognition/>*/ } <
+            ImageLinkForm onInputChange = { this.onInputChange }
+            onButtonSubmit = { this.onButtonSubmit }
+            /> <
+            FaceRecognition imageUrl = { this.state.imageUrl }
+            /> <
             /div>
         )
     }
--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './ImageLinkForm.css'
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
     return ( <
         div >
         <
@@ -14,9 +14,14 @@ const ImageLinkForm = () => {
         div className = 'form center pa4 br3 shadow-5' >
         <
         input className = 'f4 pa2 w-70 center'
-        type = 'tex' / >
+        type = 'tex'
+        onChange = { onInputChange }
+        /> <
+        button className = 'w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+        onClick = { onButtonSubmit } >
+        Detect < /button>
+
         <
-        button className = 'w-30 grow f4 link ph3 pv2 dib white bg-light-purple' > Detect < /button> <
         /div> <
         /div> <
         /div>
